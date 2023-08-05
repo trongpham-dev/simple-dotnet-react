f4c7e6f4697391abfc015f66dcf24fcd6893a6fd
trongpham <phamvantrong7.qn@gmail.com>
2023-08-05 16:57:05 +0700

    Setting up router and product page

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -7,6 +7,11 @@ import {
 import Header from "./Header";
 import Catalog from "../../features/catalog/Catalog";
 import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "../../features/home/HomePage";
+import ProductDetails from "../../features/catalog/ProductDetails";
+import AboutPage from "../../features/about/AboutPage";
+import ContactPage from "../../features/contact/ContactPage";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -28,7 +33,13 @@ function App() {
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
-        <Catalog />
+        <Routes>
+          <Route path="/" Component={HomePage} />
+          <Route path="/catalog" Component={Catalog} />
+          <Route path="/catalog/:id" Component={ProductDetails} />
+          <Route path="/about" Component={AboutPage} />
+          <Route path="/contact" Component={ContactPage} />
+        </Routes>
       </Container>
     </ThemeProvider>
   );
